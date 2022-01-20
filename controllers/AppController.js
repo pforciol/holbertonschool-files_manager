b5a218b7d85cc708d13166e3b5ecebc42bd1c113
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2022-01-20 08:48:45 +0000

    Fix ?

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,7 +5,7 @@ class AppController {
   static getStatus(req, res) {
     return res
       .status(200)
-      .json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+      .send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static async getStats(req, res) {
--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -8,25 +8,7 @@ class FilesController {
       return res.status(result.status).json(result.payload);
     }
 
-    const { name, type, parentId, isPublic, data } = req.body;
-    const files = await dbClient.database.collection('files');
-
-    if (!name) return res.status(400).json({ error: 'Missing name' });
-
-    if (!type || ['folder', 'file', 'image'].includes(type))
-      return res.status(400).json({ error: 'Missing type' });
-
-    if (!data && type != 'folder')
-      return res.status(400).json({ error: 'Missing data' });
-
-    const existingUser = await users.findOne({ email });
-    if (existingUser) return res.status(400).json({ error: 'Already exist' });
-
-    const hashedPwd = createHash('sha1').update(password).digest('hex');
-    const newUserData = { email, password: hashedPwd };
-    const result = await users.insertOne(newUserData);
-
-    return res.status(201).json({ id: result.insertedId, email });
+    return res.json('Youpi');
   }
 }
 
